1fd2555721e4b9ad50a2b9e08a688637dd678127
EJRoque <roque.ernestjohn.s.5002@gmail.com>
2024-06-07 16:58:21 +0800

    Signup page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./index.css";
 import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 
 function App() {
   return (
-    <>
-      <LoginPage />
-    </>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import amihanaLogo from "../assets/images/amihana-logo.png";
 import { HiEye, HiEyeOff } from "react-icons/hi";
 
-const LoginPage = () => {
+const SignupPage = () => {
   //Show passwordd--
   const [showPassword, setShowPassword] = useState(false);
   const togglePasswordVisibility = () => {
@@ -10,10 +10,11 @@ const LoginPage = () => {
   };
   //--
 
-  //Log in from--
+  //Sign up from--
   const [account, setAccount] = useState({
     email: "",
     password: "",
+    confirmPassword: "",
   });
 
   const handleChage = (e) => {
@@ -29,29 +30,30 @@ const LoginPage = () => {
 
     console.log("Email:", account.email);
     console.log("Password:", account.password);
+    console.log("Confirm Password:", account.confirmPassword);
 
     // Clear form after submission
     setAccount({
       email: "",
       password: "",
+      confirmPassword: "",
     });
   };
   //--
-
   return (
     //bg image
-    <div className="amihana-bg flex justify-end">
+    <div className="amihana-bg flex justify-center">
       {/* login section */}
       <div className="h-screen desktop:w-[34rem] laptop:w-[24rem] phone:w-full bg-[#E9F5FE] flex justify-center items-center flex-col">
         <div className="flex flex-col">
           {/* amina logo */}
-          <div className="desktop:w-[21rem] phone:w-[16rem] mb-5">
+          <div className="desktop:w-[21rem] phone:w-[16rem] mb-3">
             <img src={amihanaLogo} alt="Amihina logo" />
           </div>
           {/* Login form */}
           <form onSubmit={handleSubmit} className="flex flex-col">
-            <h1 className="desktop:text-4xl laptop:text-3xl phone:text-2xl font-semibold desktop:mb-5 laptop:mb-5 phone:mb-3">
-              Log in
+            <h1 className="desktop:text-4xl laptop:text-3xl phone:text-2xl font-semibold desktop:mb-5 laptop:mb-3 phone:mb-3">
+              Sign up
             </h1>
             <label
               htmlFor="email"
@@ -74,7 +76,7 @@ const LoginPage = () => {
 
             <label
               htmlFor="password"
-              className="desktop:text-2xl laptop:text-xl phone:text-lg mt-6 mb-1"
+              className="desktop:text-2xl laptop:text-xl phone:text-lg mt-5 mb-1"
             >
               Password
             </label>
@@ -99,24 +101,40 @@ const LoginPage = () => {
               </button>
             </div>
 
-            <p className="text-end mt-4 desktop:text-sm desktop:pr-1 laptop:mb-16 phone:text-xs phone:pr-1 phone:mb-10">
-              Forgot password?{" "}
-              <a href="" className="text-[#0C82B4]">
-                Click here
-              </a>
-            </p>
+            <label
+              htmlFor="confirmPassword"
+              className="desktop:text-2xl laptop:text-xl phone:text-lg mt-5 mb-1"
+            >
+              Confirm Password
+            </label>
+            <div className="desktop:w-[21rem] desktop:h-[3rem] phone:w-[16rem] phone:h-[2.7rem] bg-white border-2 border-solid border-gray-400 rounded-md flex items-center relative ${formErrors.password && 'mb-2'}">
+              <input
+                required
+                pattern={account.password}
+                title="Password does not match"
+                type={showPassword ? "text" : "password"}
+                id="confirmPassword"
+                name="confirmPassword"
+                value={account.confirmPassword}
+                onChange={handleChage}
+                placeholder="Enter password"
+                className="flex-grow px-4 pr-10 h-[2rem] outline-none"
+              />
+              <button
+                type="button"
+                className="absolute inset-y-0 right-0 flex items-center justify-center px-3"
+                onClick={togglePasswordVisibility}
+              >
+                {showPassword ? <HiEyeOff /> : <HiEye />}
+              </button>
+            </div>
+
             <button
               type="submit"
-              className="desktop:h-[3rem] desktop:w-[21rem] phone:h-[2.7rem] phone:w-[16rem] bg-[#0C82B4] rounded-md text-white mb-5"
+              className="desktop:h-[3rem] desktop:w-[21rem] phone:h-[2.7rem] phone:w-[16rem] bg-[#0C82B4] rounded-md text-white mt-16"
             >
-              Log in
+              Sign up
             </button>
-            <p className="text-center desktop:text-sm phone:text-xs">
-              Don't have an account yet?{" "}
-              <a href="/signup" className="text-[#0C82B4]">
-                Sign up here
-              </a>
-            </p>
           </form>
         </div>
       </div>
@@ -124,4 +142,4 @@ const LoginPage = () => {
   );
 };
 
-export default LoginPage;
+export default SignupPage;
