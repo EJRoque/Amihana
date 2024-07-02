ca8b4a56f2c25aa6127d81d9741751c8af0c2e1c
EJRoque <roque.ernestjohn.s.5002@gmail.com>
2024-07-02 17:33:14 +0800

    Profile pic centered

--- src/pages/OnboardingPage.jsx
+++ src/pages/OnboardingPage.jsx
@@ -42,56 +42,57 @@ const OnboardingPage = ({
     <div className="amihana-bg flex justify-center">
       {/* onboarding section */}
       <div className="h-screen desktop:w-[34rem] laptop:w-[24rem] phone:w-full bg-[#E9F5FE] flex justify-center items-center flex-col">
-        <div className="flex flex-col">
+        <div className="flex justify-center items-center flex-col">
           {/* amihana logo */}
-          <div className="desktop:w-[21rem] phone:w-[16rem] mb-1">
+          <div className="flex desktop:w-[18rem] laptop:w-[14rem] phone:w-[12rem] mb-1">
             <img src={amihanaLogo} alt="Amihina logo" />
           </div>
           {/* Onboarding form */}
           <form onSubmit={handleSubmit} className="flex flex-col">
-            <h1 className="text-center desktop:text-2xl laptop:text-xl phone:text-xl font-semibold desktop:mb-5 laptop:mb-3 phone:mb-3">
+            <h1 className="text-center desktop:text-2xl laptop:text-xl phone:text-xl font-semibold mb-2">
               Lets create your account
             </h1>
 
             <label
               htmlFor="profilePicture"
-              className="desktop:text-2xl laptop:text-xl phone:text-lg mb-1"
+              className="text-center desktop:text-xl laptop:text-lg phone:text-lg mb-2"
             >
               Profile picture
             </label>
-            <div className="desktop:w-[21rem] desktop:h-[3rem] phone:w-[16rem] phone:h-[2.5rem] bg-white border-2 border-solid border-gray-400 rounded-md flex items-center">
+            <div className="h-[6rem] desktop:w-[21rem] laptop:w-[16rem] phone:w-[16rem] flex flex-col items-center justify-center">
               {imagePreview ? (
                 <img
                   src={imagePreview}
                   alt="Profile Preview"
-                  className="desktop:w-10 desktop:h-10 phone:w-8 phone:h-8 object-cover rounded-full ml-4 mr-1"
+                  className="desktop:w-16 desktop:h-16 laptop:w-14 laptop:h-14 phone:w-12 phone:h-12 object-cover rounded-full mb-1"
                 />
               ) : (
                 <img
                   src={defaultProfilePic}
                   alt="Default Profile Preview"
-                  className="desktop:w-10 desktop:h-10 phone:w-8 phone:h-8 object-cover rounded-full ml-4 mr-1"
+                  className="desktop:w-16 desktop:h-16 laptop:w-14 laptop:h-14 phone:w-12 phone:h-12 object-cover rounded-full mb-1"
                 />
               )}
+
               <input
                 required
                 type="file"
                 id="profilePicture"
+                AAAAAAAAA
                 name="profilePicture"
                 accept=".jpg, .jpeg, .png"
                 onChange={handleChange}
-                className="flex-grow h-full outline-none file:mr-2 file:py-1 file:px-2 file:border-0 file:text-sm file:bg-gray-200 file:text-gray-700 file:rounded-md"
-                style={{ height: "auto" }}
+                className="ml-20 phone:w-[16rem] phone:ml-10 file:mr-2 file:py-1 file:px-2 file:border-0 file:text-sm file:bg-gray-200 file:text-gray-700 file:rounded-md"
               />
             </div>
 
             <label
               htmlFor="fullName"
-              className="desktop:text-2xl laptop:text-xl phone:text-lg mt-3 mb-1"
+              className="desktop:text-xl laptop:text-xl phone:text-lg mb-1"
             >
               Full name
             </label>
-            <div className="desktop:w-[21rem] desktop:h-[3rem] phone:w-[16rem] phone:h-[2.5rem] bg-white border-2 border-solid border-gray-400 rounded-md flex items-center">
+            <div className="h-[2.5rem] desktop:w-[21rem] phone:w-[16rem] bg-white border-2 border-solid border-gray-400 rounded-md flex items-center">
               <input
                 required
                 type="text"
@@ -100,17 +101,17 @@ const OnboardingPage = ({
                 value={account.fullName}
                 onChange={handleChange}
                 placeholder="ex. Juan Dela Cruz"
-                className="flex-grow px-4 pr-10 h-[2rem] outline-none"
+                className="flex-grow px-4 pr-10 h-[1rem] outline-none"
               />
             </div>
 
             <label
               htmlFor="phoneNumber"
-              className="desktop:text-2xl laptop:text-xl phone:text-lg mt-3 mb-1"
+              className="desktop:text-xl laptop:text-xl phone:text-lg mt-3 mb-1"
             >
               Phone number
             </label>
-            <div className="desktop:w-[21rem] desktop:h-[3rem] phone:w-[16rem] phone:h-[2.5rem] bg-white border-2 border-solid border-gray-400 rounded-md flex items-center relative">
+            <div className="h-[2.5rem] desktop:w-[21rem] phone:w-[16rem] bg-white border-2 border-solid border-gray-400 rounded-md flex items-center">
               <input
                 required
                 minLength="11"
@@ -126,11 +127,11 @@ const OnboardingPage = ({
 
             <label
               htmlFor="age"
-              className="desktop:text-2xl laptop:text-xl phone:text-lg mt-3 mb-1"
+              className="desktop:text-xl laptop:text-xl phone:text-lg mt-3 mb-1"
             >
               Age
             </label>
-            <div className="desktop:w-[21rem] desktop:h-[3rem] phone:w-[16rem] phone:h-[2.5rem] bg-white border-2 border-solid border-gray-400 rounded-md flex items-center relative">
+            <div className="h-[2.5rem] desktop:w-[21rem] phone:w-[16rem] bg-white border-2 border-solid border-gray-400 rounded-md flex items-center">
               <input
                 required
                 type="text"
@@ -145,7 +146,7 @@ const OnboardingPage = ({
 
             <button
               type="submit"
-              className="desktop:h-[3rem] desktop:w-[21rem] phone:h-[2.5rem] phone:w-[16rem] bg-[#0C82B4] rounded-md text-white desktop:mt-16 mt-10"
+              className="h-[2.5rem] desktop:w-[21rem] phone:w-[16rem] bg-[#0C82B4] rounded-md text-white desktop:mt-14 mt-10"
             >
               Next
             </button>
