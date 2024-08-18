fed491ebf9fddd309bdb03019239dd452b0b9b4e
EJRoque <roque.ernestjohn.s.5002@gmail.com>
2024-08-18 18:45:34 +0800

    phase, block, lot added

--- src/pages/ReviewOnboardingPage.jsx
+++ src/pages/ReviewOnboardingPage.jsx
@@ -38,6 +38,9 @@ const ReviewOnboardingPage = ({ account, imagePreview }) => {
         fullName: account.fullName,
         phoneNumber: account.phoneNumber,
         age: account.age,
+        phase: account.phase,
+        block: account.block,
+        lot: account.lot,
       });
 
       console.log("Document successfully updated!");
@@ -53,16 +56,16 @@ const ReviewOnboardingPage = ({ account, imagePreview }) => {
     // bg image
     <div className="amihana-bg flex justify-center">
       {/* onboarding section */}
-      <div className="h-screen desktop:w-[34rem] laptop:w-[24rem] phone:w-full bg-[#E9F5FE] flex justify-center items-center flex-col">
+      <div className="min-h-screen desktop:w-[34rem] laptop:w-[34rem] phone:w-full bg-[#E9F5FE] flex justify-center items-center flex-col">
         <div className="flex justify-center items-center flex-col">
           {/* amihana logo */}
           <div className="flex desktop:w-[18rem] laptop:w-[14rem] phone:w-[12rem] mb-1">
             <img src={amihanaLogo} alt="Amihina logo" />
           </div>
           {/* Onboarding form */}
-          <form onSubmit={handleSubmit} className="flex flex-col">
+          <form onSubmit={handleSubmit} className="flex flex-col items-center">
             <h1 className="text-center desktop:text-2xl laptop:text-xl phone:text-xl font-semibold mb-2">
-              Lets review your details
+              Let's review your details
             </h1>
 
             <label
@@ -71,57 +74,80 @@ const ReviewOnboardingPage = ({ account, imagePreview }) => {
             >
               Profile picture
             </label>
-            <div className="h-[6rem] desktop:w-[21rem] laptop:w-[16rem] phone:w-[16rem] flex flex-col items-center justify-center">
-              <img
-                src={imagePreview}
-                alt="Profile Preview"
-                className="desktop:w-16 desktop:h-16 laptop:w-14 laptop:h-14 phone:w-12 phone:h-12 object-cover rounded-full mb-1"
-              />
+            <div className="flex justify-center items-center mb-4">
+              <div className="h-[6rem] w-[6rem] flex items-center justify-center">
+                <img
+                  src={imagePreview}
+                  alt="Profile Preview"
+                  className="desktop:w-16 desktop:h-16 laptop:w-14 laptop:h-14 phone:w-12 phone:h-12 object-cover rounded-full"
+                />
+              </div>
             </div>
 
-            <label
-              htmlFor="fullName"
-              className="desktop:text-xl laptop:text-xl phone:text-lg mb-1"
-            >
-              Full name
-            </label>
-            <div className="h-[2.5rem] desktop:w-[21rem] phone:w-[16rem] bg-white border-2 border-solid border-gray-400 rounded-md flex items-center">
-              <p className="ml-4">{account.fullName}</p>
+            <div className="grid grid-cols-1 gap-4 mt-4 tablet:grid-cols-2">
+              <div>
+                <label className="desktop:text-xl laptop:text-xl phone:text-lg mb-1 ml-1">
+                  Full name
+                </label>
+                <div className="h-[2.5rem] desktop:w-[13.5rem] laptop:w-[13.5rem] phone:w-[16rem] bg-white border-2 border-solid border-gray-400 rounded-md flex items-center">
+                  <p className="ml-4">{account.fullName}</p>
+                </div>
+              </div>
+              <div>
+                <label className="desktop:text-xl laptop:text-xl phone:text-lg mt-3 mb-1 ml-1">
+                  Phone number
+                </label>
+                <div className="h-[2.5rem] bg-white border-2 border-solid border-gray-400 rounded-md flex items-center">
+                  <p className="ml-4">{account.phoneNumber}</p>
+                </div>
+              </div>
+              <div>
+                <label className="desktop:text-xl laptop:text-xl phone:text-lg mt-3 mb-1 ml-1">
+                  Age
+                </label>
+                <div className="h-[2.5rem] bg-white border-2 border-solid border-gray-400 rounded-md flex items-center">
+                  <p className="ml-4">{account.age}</p>
+                </div>
+              </div>
+              <div>
+                <label className="desktop:text-xl laptop:text-xl phone:text-lg mt-3 mb-1 ml-1">
+                  Phase
+                </label>
+                <div className="h-[2.5rem] bg-white border-2 border-solid border-gray-400 rounded-md flex items-center">
+                  <p className="ml-4">{account.phase}</p>
+                </div>
+              </div>
+              <div>
+                <label className="desktop:text-xl laptop:text-xl phone:text-lg mt-3 mb-1 ml-1">
+                  Block
+                </label>
+                <div className="h-[2.5rem] bg-white border-2 border-solid border-gray-400 rounded-md flex items-center">
+                  <p className="ml-4">{account.block}</p>
+                </div>
+              </div>
+              <div>
+                <label className="desktop:text-xl laptop:text-xl phone:text-lg mt-3 mb-1 ml-1">
+                  Lot
+                </label>
+                <div className="h-[2.5rem] bg-white border-2 border-solid border-gray-400 rounded-md flex items-center">
+                  <p className="ml-4">{account.lot}</p>
+                </div>
+              </div>
             </div>
 
-            <label
-              htmlFor="phoneNumber"
-              className="desktop:text-xl laptop:text-xl phone:text-lg mt-3 mb-1"
-            >
-              Phone number
-            </label>
-            <div className="h-[2.5rem] desktop:w-[21rem] phone:w-[16rem] bg-white border-2 border-solid border-gray-400 rounded-md flex items-center">
-              <p className="ml-4">{account.phoneNumber}</p>
-            </div>
-
-            <label
-              htmlFor="age"
-              className="desktop:text-xl laptop:text-xl phone:text-lg mt-3 mb-1"
-            >
-              Age
-            </label>
-            <div className="h-[2.5rem] desktop:w-[21rem] phone:w-[16rem] bg-white border-2 border-solid border-gray-400 rounded-md flex items-center">
-              <p className="ml-4">{account.age}</p>
-            </div>
-
-            <div className="flex desktop:space-x-28 desktop:mt-14 mt-10 phone:space-x-24">
+            <div className="flex w-full justify-between my-5">
               <button
                 type="button"
                 //this will go back to the previous page
                 onClick={() => navigate(-1)}
-                className="h-[2.5rem] desktop:w-[7rem] phone:w-[5rem] bg-white rounded-md text-[#0C82B4] border-2 border-solid border-gray-400"
+                className="h-[2.5rem] desktop:w-[7rem] laptop:w-[7rem] phone:w-[5rem] bg-white rounded-md text-[#0C82B4] border-2 border-solid border-gray-400"
               >
                 Back
               </button>
 
               <button
                 type="submit"
-                className="h-[2.5rem] desktop:w-[7rem] phone:w-[5rem] bg-[#0C82B4] rounded-md text-white"
+                className="h-[2.5rem] desktop:w-[7rem] laptop:w-[7rem] phone:w-[5rem] bg-[#0C82B4] rounded-md text-white"
               >
                 Submit
               </button>
