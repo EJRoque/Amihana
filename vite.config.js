607d64de3fbd9e84ca3df24a879de392520be843
EJRoque <roque.ernestjohn.s.5002@gmail.com>
2024-09-13 16:38:52 +0800

    fix vercel 2

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   server: {
     host: false, // This allows access from other devices on the network
   },
