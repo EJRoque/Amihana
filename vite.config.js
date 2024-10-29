c8f374b852b4c47557b1bc45b1392aa81da36d8e
EJRoque <roque.ernestjohn.s.5002@gmail.com>
2024-10-30 02:40:10 +0800

    for vercel langs

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: false, // This allows access from other devices on the networ
+    host: false, // This allows access from other devices on the network
   },
 });
